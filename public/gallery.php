0b72b2dfeae1016376ad7b87f7a88c0a8583a6c0
Qvot <qvot.denis@ya.ru>
2019-06-17 14:09:21 +0300

    подключение вывода галереи

--- public/gallery.php
+++ public/gallery.php
@@ -3,11 +3,11 @@
 require_once __DIR__ . '/../config/config.php';
 
 
-var_dump(scandir(WWW_DIR . IMG_DIR));
-die;
 
-echo render(TEMPLATES_DIR . 'index.tpl', [
-	'title' => 'Галерея',
-	'h1' => 'Галерея',
-	'content' => render(TEMPLATES_DIR . 'gallery.tpl')
+
+
+echo render( TEMPLATES_DIR . 'index.tpl', [
+	'title'		=> 'Галерея',
+	'h1'		=> 'Галерея',
+	'content'	=> gallery( 'img' )
 ]);
